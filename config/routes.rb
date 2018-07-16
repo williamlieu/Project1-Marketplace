21288273c12ac3cf2b21bb7bd255c98801e5a0f7
williamlieu <william.lieu@outlook.fr>
2018-07-16 17:02:02 +1000

    secondcom

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
   root :to => "pages#home"
   resources :products, :only => [:index, :new, :create, :edit, :update]
+  resources :users
+
+  get '/login' => 'session#new' # login form
+  post '/login' => 'session#create' # perform a login
+  delete '/login' => 'session#destroy' # perform a log out
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_15_034015) do
+ActiveRecord::Schema.define(version: 2018_07_16_061839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 2018_07_15_034015) do
   create_table "products", force: :cascade do |t|
     t.text "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
